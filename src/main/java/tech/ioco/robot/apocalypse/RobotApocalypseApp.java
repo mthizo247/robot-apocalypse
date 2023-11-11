4f41ee20ce0f675335543f42d90934cf8898c511
Ronald Mthombeni <ronald.mthombeni@momentum.co.za>
2023-11-11 19:15:20 +0200

    init

--- src/main/java/tech/ioco/robot/apocalypse/RobotApocalypseApp.java
+++ src/main/java/tech/ioco/robot/apocalypse/RobotApocalypseApp.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 //@EnableCaching
 @RestController
 @Hidden
+
 public class RobotApocalypseApp {
     @SuppressWarnings("all")
     public static void main(String[] args) throws Exception {
